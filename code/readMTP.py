eaf1c10738b7800ed5a8a67d7d34a3e93489c3d9
Anshuman Suri <iamgroot42@users.noreply.github.com>
2018-12-09 15:46:49 +0530

    Update readMTP.py

--- code/readMTP.py
+++ code/readMTP.py
@@ -7,79 +7,94 @@ from tqdm import tqdm
 
 def readAllImages(dirPath):
 	person_wise = {}
-	for path in tqdm(os.listdir(dirPath)):
+	people = []
+	for path in os.listdir(dirPath):
+		person_id = int(path.split('_')[0])
 		if "_051_06" in path or "_051_08" in path:
+			if person_id not in person_wise:
+				person_wise[person_id] = []
+			person_wise[person_id].append(path)
+	person_keys = person_wise.keys()
+	for key in tqdm(person_keys):
+		temp_people = []
+		for person in person_wise[key]:
 			img = cv2.resize(np.asarray(Image.open(os.path.join(dirPath, path)).convert('RGB'), dtype=np.float32), (150, 150))
-			person_id = int(path.split('_')[0])
-			person_wise[person_id] = person_wise.get(person_id, []) + [img]
-	return person_wise
-
-
-def personsplit(person_wise, split_ratio=0.25, target_resolution=(48, 48), num_train=100):
-	shuffled_indices = np.random.choice(person_wise.keys(), len(person_wise.keys()), replace=False)
-	train_indices =shuffled_indices[:num_train]
-	test_indices  = shuffled_indices[num_train:]
-
-	# Make splits for labelled/unlabelled data
-	split_pre, split_post = {}, {}
-	for i in range(len(train_indices)):
-		splitPoint = int(len(person_wise[train_indices[i]]) * split_ratio)
-		indices = np.arange(len(person_wise[train_indices[i]]))
-		if train_indices[i] in split_pre:
-			split_pre[train_indices[i]]  = np.concatenate((split_pre[train_indices[i]], np.array(person_wise[train_indices[i]])[indices[:splitPoint]]))
-			split_post[train_indices[i]] = np.concatenate((split_post[train_indices[i]],np.array(person_wise[train_indices[i]])[indices[splitPoint:]]))
-		else:
-			split_pre[train_indices[i]]  = np.array(person_wise[train_indices[i]])[indices[:splitPoint]]
-			split_post[train_indices[i]] = np.array(person_wise[train_indices[i]])[indices[splitPoint:]]
-
-	# Contruct siamese-compatible data
-	def mix_match_data(data, base, resize=False):
-		data_X_left, data_X_right, data_Y = [], [], []
-		for i in tqdm(range(len(data))):
-			# All images of that person
-			for i_sub in base[data[i]]:
-				if resize:
-					i_sub_smaller = cv2.resize(i_sub, target_resolution)
-				else:
-					i_sub_smaller = i_sub
-				for j in range(i, len(data)):
-					# All images of that person
-					for j_sub in base[data[j]]:
-						if resize:
-							j_sub_smaller = cv2.resize(j_sub, target_resolution)
-						else:
-							j_sub_smaller = j_sub
-						data_X_left.append(i_sub_smaller)
-						data_X_right.append(j_sub_smaller)
-						if i == j:
-							data_Y.append([1])
-						else:
-							data_Y.append([0])
-		return (data_X_left, data_X_right), data_Y
-
-	train_X, train_Y = mix_match_data(train_indices, split_pre)
-	pool_X, pool_Y   = mix_match_data(train_indices, split_post)
-	# TODO : Use only the test data that they used (for consistent comparison)
-	test_X, test_Y   = mix_match_data(test_indices, person_wise, resize=True)
-
-	# Free up RAM
-	del person_wise
-
-	return (train_X, train_Y), (pool_X, pool_Y), (test_X, test_Y)
+			temp_people.append(img)
+		people.append(np.stack(temp_people))
+	return people
 
 
 def generatorFeaturized(X, Y, batch_size, featurize=None, resize_res=None):
+	X_left, X_right, Y_send = [], [], []
 	while True:
 		for i in range(0, len(X), batch_size):
-			X_left  = X[0][i: i + batch_size]
-			X_right = X[1][i: i + batch_size]
-			Y       = Y[i: i + batch_size]
+			x_left  = np.array(X[0][i: i + batch_size])
+			x_right = np.array(X[1][i: i + batch_size])
+			Y       = np.array(Y[i: i + batch_size])
+			# De-bias data
+			Y_flat  = np.stack([y[0] for y in Y])
+			pos = np.where(Y_flat == 1)[0]
+			neg = np.where(Y_flat == 0)[0]
+			# Don't train on totally biased data
+			minSamp = np.minimum(len(pos), len(neg))
+			if minSamp == 0:
+				continue
+			selectedIndices = np.concatenate((np.random.choice(pos, minSamp, replace=False), np.random.choice(neg, minSamp, replace=False)), axis=0)
+			Y = Y[selectedIndices]
+			x_left, x_right = x_left[selectedIndices], x_right[selectedIndices]
+			# Resize, if asked to
 			if resize_res:
-				X_left, X_right = resizeImages([X_left, X_right], resize_res)
+				x_left, x_right = resizeImages([x_left, x_right], resize_res)
+			# Featurize, if asked to
 			if featurize:
-				X_left = featurize.process(X_left)
-				X_right = featurize.process(X_right)
-			yield [X_left, X_right], Y
+				x_left  = featurize.process(x_left)
+				x_right = featurize.process(x_right)
+			if len(Y_send) > 0:
+				X_left = np.concatenate((X_left, x_left), axis=0)
+				X_right = np.concatenate((X_right, x_right), axis=0)
+				Y_send = np.concatenate((Y_send, Y), axis=0)
+			else:
+				X_left = np.copy(x_left)
+				X_right = np.copy(x_right)
+				Y_send = np.copy(Y)
+			if len(Y_send) >= batch_size:
+				yield ([X_left, X_right], Y_send)
+				X_left, X_right, Y_send = [], [], []
+
+
+def getGenerator(datGen, batch_size, val_ratio=0.2, resize_res=None, featurize=None):
+	X_left, X_right, Y_send = [], [], []
+	while True:
+		X, Y = datGen.next()
+		# Generate data in 1:1 ratio to avoid overfitting
+		Y_flat = np.stack([y[0] for y in Y])
+		pos = np.where(Y_flat == 1)[0]
+		neg = np.where(Y_flat == 0)[0]
+		minSamp = np.minimum(len(pos), len(neg))
+		# Don't train on totally biased data
+		if minSamp == 0:
+			continue
+		selectedIndices = np.concatenate((np.random.choice(pos, minSamp, replace=False), np.random.choice(neg, minSamp, replace=False)), axis=0)
+		Y = Y[selectedIndices]
+		X = [X[0][selectedIndices], X[1][selectedIndices]]
+		# Resize, if asked to
+		if resize_res:
+			X[0], X[1] = resizeImages([X[0], X[1]], resize_res)
+		# Featurize, if asked to
+		if featurize:
+			X[0]  = featurize.process(X[0])
+			X[1] = featurize.process(X[1])
+		if len(Y_send) > 0:
+			X_left = np.concatenate((X_left, X[0]), axis=0)
+			X_right = np.concatenate((X_right, X[1]), axis=0)
+			Y_send = np.concatenate((Y_send, Y), axis=0)
+		else:
+			X_left = np.copy(X[0])
+			X_right = np.copy(X[1])
+			Y_send = np.copy(Y)
+		if len(Y_send) >= batch_size:
+			yield ( [X_left, X_right], Y_send)
+			X_left, X_right, Y_send = [], [], []
 
 
 def resizeImages(images, resize_res):
